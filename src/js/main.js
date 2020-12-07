f0939a39e00b56d5fbc7fd7c1299639f25380265
LKosharnaya <llyashkova@gmail.com>
2020-12-07 20:42:47 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import './slider';
+import modals from './modules/modal';
+import tabs from './modules/tabs';
+import forms from './modules/forms';
+import CangeModals from './modules/changeModal';
+
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+    "use strict";
+    let modalState = {};
+
+    CangeModals(modalState);
+    modals(modalState);
+    tabs('.glazing_slider','.glazing_block','.glazing_content','active');
+    tabs('.decoration_slider','.no_click','.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img','.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
+});
